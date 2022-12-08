adcbb149c4de395481090d46e342c4c303de3a96
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-08 15:50:58 -0600

    finished syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my personal string";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x = x += 5;
+//        System.out.println(x);
+
+
+
+
+
     }
 }
