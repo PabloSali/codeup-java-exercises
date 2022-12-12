b8cacc7e9a0e3a77b0a9a0f7a3d68dd9814fc0dd
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-12 10:38:49 -0600

    finished console exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main (String[] args){
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.\n", pi);
+        //-------------------
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Please enter an integer:");
+//        int userInput = scanner.nextInt();
+//        System.out.println("The number you entered was: " + userInput);
+
+        //------------------
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your first word: ");
+//        String firstStr = scanner.nextLine();
+//
+//        System.out.println("Enter your second word: ");
+//        String secondStr = scanner.nextLine();
+//
+//        System.out.println("Enter your third word: ");
+//        String thirdStr = scanner.nextLine();
+//
+//        System.out.println(firstStr);
+//        System.out.println(secondStr);
+//        System.out.println(thirdStr);
+
+        //-------------------
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a full sentence.");
+//        String userSentence = scanner.nextLine();
+//        System.out.println("Your sentence was: " + userSentence);
+
+        //--------------------
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the length of your classroom: ");
+//        String userLth = scanner.nextLine();
+//        int roomLth = Integer.parseInt(userLth);
+//        System.out.println("Enter the width of your classroom: ");
+//        String userWth = scanner.nextLine();
+//        int roomWth = Integer.parseInt(userWth);
+//        int roomPerimeter = ((roomLth * 2) + (roomWth * 2));
+//        int roomArea = (roomLth * roomWth);
+//        System.out.println("The perimeter of your room is: " + roomPerimeter + " and the Area of your room is: " + roomArea);
+    }
 }
