fbf705b21ea0fe341ada76237a7e4c11b8143d49
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-14 14:25:01 -0600

    completed Q1 and Q2

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,53 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        //1 a
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //1 b
+
+//        int count = 0;
+//        do{
+//            if(count % 2 == 0)
+//                System.out.println(count);
+//            count++;
+//        }while(count <= 100);
+
+
+        // 1 c
+//        int count = 100;
+//        do{
+//            if(count % 5 == 0){
+//                System.out.println(count);
+//            }
+//            count -= 5;
+//        }while(count >= -10);
+
+        // 2
+//        int i = 1;
+//
+//        do{
+//            if(i % 5 == 0 && i % 3 == 0){
+//                System.out.println("FizzBuzz");
+//                i++;
+//            }
+//            else if(i % 5 == 0){
+//                System.out.println("Fizz");
+//                i++;
+//            }
+//            else if(i % 3 == 0){
+//                System.out.println("Buzz");
+//                i++;
+//            }
+//            else{
+//                i++;
+//            }
+//        }while(i <= 100);
+
+        //3
+
+    }
+}
