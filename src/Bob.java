4c86a50a644c6e1c665d17c5d275137b73785eb7
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-15 11:21:38 -0600

    completed Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        System.out.print("This is Bob! Feel free to converse: ");
+        String userInput = user.next();
 
+        if(userInput.endsWith("?")){
+            System.out.print("Sure.");
+        } else if(userInput.endsWith("!")){
+            System.out.print("Whoa, chill out!");
+        }else if (userInput.equals("")){
+            System.out.print("Fine be that way");
+        }else{
+            System.out.println("Whatever");
+        }
     }
 }
