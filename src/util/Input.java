b95367f6ca5591a59d2e683ce8f9fd2d4b06839f
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-02 11:16:11 -0600

    completed exercise 1 in OOP

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,5 +4,41 @@ import java.util.Scanner;
 public class Input {
     private static Scanner scanner = new Scanner(System.in);
 
+    public static String getString(){
+        return scanner.nextLine();
+    }
 
+    public static boolean yesNo(){
+        System.out.print("select [y/n]: ");
+        String userInput = scanner.nextLine();
+        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y");
+    }
+
+    public static int getInt(int min, int max){
+        System.out.println("Enter a number between " + min + "and " + max + ":");
+        int enteredNum = scanner.nextInt();
+        if(enteredNum > max || enteredNum < min){
+            System.out.println(enteredNum + " is not between " + min + "and " + max + ".");
+            getInt(min,max);
+        }
+        return enteredNum;
+    }
+
+    public static int getInt() {
+        return scanner.nextInt();
+    }
+
+    double getDouble(double min, double max){
+        System.out.println("Enter a number between " + min + "and " + max + ":");
+        int enteredNum = scanner.nextInt();
+        if(enteredNum > max || enteredNum < min){
+            System.out.println(enteredNum + " is not between " + min + "and " + max + ".");
+            getDouble(min,max);
+        }
+        return enteredNum;
+    }
+
+    public static int getDouble() {
+        return scanner.nextInt();
+    }
 }
