c8d0a394dc37f1a2ee79c5d87d11622b10dd65f3
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-02 11:24:27 -0600

    completed making circle class requirements' git push

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,16 @@
 package shapes;
-
 public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI * (radius * radius);
+    }
+
+    public double getCircumference(){
+        return Math.PI * 2 * radius;
+    }
 }
