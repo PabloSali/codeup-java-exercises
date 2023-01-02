a9133df0fc24a1a2893f7925fa4c206cecb4e003
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-02 13:39:49 -0600

    completed exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,5 +1,13 @@
 package shapes;
+import util.Input;
 
 public class CircleApp {
-
+    public static void main(String[] args) {
+        System.out.print("Input a radius: ");
+        double radiusInput = Input.getDouble();
+        Circle circle = new Circle(radiusInput);
+        double area = circle.getArea();
+        double circumference = circle.getCircumference();
+        System.out.printf("Your Circle has an area of %s, and a circumference of %s", area, circumference);
+    }
 }
