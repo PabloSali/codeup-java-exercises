da65b0ea572c20f7c2f914d3bf8d5231c7545cc2
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-03 11:18:09 -0600

    Created rectangle exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+    }
+    int area;
+
+    public void getArea(int length, int width){
+        this.area = length * width;
+    }
+
+    public void getPerimeter(int length, int width){
+        this.area = length + width;
+    }
+}
