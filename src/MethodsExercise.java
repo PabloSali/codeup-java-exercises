09e7cf46cd1e674257d8b3d5e6774c42efb03600
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-05 07:15:01 -0600

    refactors methods exercise

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MethodsExercises {
+public class MethodsExercise {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // 1
