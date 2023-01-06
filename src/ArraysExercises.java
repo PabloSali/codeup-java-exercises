8950cfe828761c46d14f2ad81eece16ed644e541
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-06 12:18:59 -0600

    completed exercise 2

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -6,9 +6,25 @@ public class ArraysExercises {
         arrayPerson[0] = new Person ("Jimothy");
         arrayPerson[1] = new Person ("Rimothy");
         arrayPerson[2] = new Person ("Dimothy");
+//        for(Person person: arrayPerson){
+//            System.out.println(person.name);
+//        }
 
-        for(Person person: arrayPerson){
-            System.out.println(person.name);
+        Person newPerson = new Person ("Jebediah");
+
+        addPerson(arrayPerson, newPerson);
+    }
+    static void addPerson(Person[] arrayPerson,Person personAdding){
+        int startingArrayLeng = arrayPerson.length;
+        Person[] newArray = new Person[startingArrayLeng + 1];
+        int newArrayLeng = newArray.length;
+        newArray[newArrayLeng - 1] = personAdding;
+
+        for(int i = 0; i < newArrayLeng; i++){
+            if(i < newArrayLeng -1) {
+                newArray[i] = arrayPerson[i];
+            }
+            System.out.println(newArray[i].name);
         }
     }
 }
